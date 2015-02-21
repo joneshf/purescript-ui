6075df5d4c461c599c14096baca413320c79888a
joneshf <jones3.hardy@gmail.com>
2015-02-21 09:39:06 -0800

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+'use strict'
+
+var gulp        = require('gulp')
+  , bump        = require('gulp-bump')
+  , filter      = require('gulp-filter')
+  , git         = require('gulp-git')
+  , purescript  = require('gulp-purescript')
+  , runSequence = require('run-sequence')
+  , tagVersion  = require('gulp-tag-version')
+  ;
+
+var paths = {
+    src: 'src/**/*.purs',
+    bowerSrc: 'bower_components/purescript-*/src/**/*.purs',
+    dest: '',
+    docsDest: 'README.md',
+    manifests: [
+      'bower.json',
+      'package.json'
+    ]
+};
+
+var options = {
+    compiler: {},
+    pscDocs: {}
+};
+
+var compile = function(compiler) {
+    var psc = compiler(options.compiler);
+    psc.on('error', function(e) {
+        console.error(e.message);
+        psc.end();
+    });
+    return gulp.src([paths.src, paths.bowerSrc])
+        .pipe(psc)
+        .pipe(gulp.dest(paths.dest));
+};
+
+function bumpType(type) {
+    return gulp.src(paths.manifests)
+        .pipe(bump({type: type}))
+        .pipe(gulp.dest('./'));
+}
+
+gulp.task('tag', function() {
+    return gulp.src(paths.manifests)
+        .pipe(git.commit('Update versions.'))
+        .pipe(filter('bower.json'))
+        .pipe(tagVersion());
+});
+
+gulp.task('bump-major', function() {
+    return bumpType('major')
+});
+gulp.task('bump-minor', function() {
+    return bumpType('minor')
+});
+gulp.task('bump-patch', function() {
+    return bumpType('patch')
+});
+
+gulp.task('bump-tag-major', function() {
+    return runSequence('bump-major', 'tag');
+});
+gulp.task('bump-tag-minor', function() {
+    return runSequence('bump-minor', 'tag');
+});
+gulp.task('bump-tag-patch', function() {
+    return runSequence('bump-patch', 'tag');
+});
+
+gulp.task('make', function() {
+    return compile(purescript.pscMake);
+});
+
+gulp.task('browser', function() {
+    return compile(purescript.psc);
+});
+
+gulp.task('docs', function() {
+    var pscDocs = purescript.pscDocs(options.pscDocs);
+    pscDocs.on('error', function(e) {
+        console.error(e.message);
+        pscDocs.end();
+    });
+    return gulp.src(paths.src)
+      .pipe(pscDocs)
+      .pipe(gulp.dest(paths.docsDest));
+});
+
+gulp.task('watch-browser', function() {
+    gulp.watch(paths.src, ['browser', 'docs']);
+});
+
+gulp.task('watch-make', function() {
+    gulp.watch(paths.src, ['make', 'docs']);
+});
+
+gulp.task('default', ['make', 'docs']);
